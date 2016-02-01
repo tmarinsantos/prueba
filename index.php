038f42f05dbb86809e09e89a42e7c98df9c30232
tonim <marinsantosam@gmail.com>
2016-02-01 18:59:02 +0100

    modificacion index

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
 <meta charset="utf-8">
 </head>
 <body>
-<h1>Usuarios de la aplicación</h1>
-<?php
-$dbuser = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-$dbpassword = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-$dbname = getenv("OPENSHIFT_APP_NAME");11
-mysql_connect($dbhost, $dbuser, $dbpassword) or die(mysql_error());
-mysql_select_db($dbname) or die(mysql_error());
-$result = mysql_query("SELECT * FROM users") or die(mysql_error());
-?>
-<table border cellpadding=3>
-<tr>
-<td>Nombre Usuario</td>
-<td>Dirección email</td>
-</tr>
-<?php
-while($currentRow = mysql_fetch_array($result)){
-echo "<tr>";
-echo "<td>" . $currentRow['nombre'] . "</td>";
-echo "<td>" . $currentRow['apellidos'] . "</td>";
-echo "</tr>";
-}
-?>
-</table>
+	<h1>Usuarios de la aplicación</h1>
+	<?php
+		$dbhost = getenv("OPENSHIFT_MYSQL_DB_HOST");
+		$dbuser = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+		$dbpassword = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+		$dbname = getenv("OPENSHIFT_APP_NAME");
+
+		mysql_connect($dbhost, $dbuser, $dbpassword) or die(mysql_error());
+		mysql_select_db($dbname) or die(mysql_error());
+		$result = mysql_query("SELECT * FROM users") or die(mysql_error());
+	?>
+	<table border cellpadding=3>
+		<tr>
+		<td>Nombre Usuario</td>
+		<td>Dirección email</td>
+		</tr>
+		<?php
+		while($currentRow = mysql_fetch_array($result)){
+		echo "<tr>";
+		echo "<td>" . $currentRow['nombre'] . "</td>";
+		echo "<td>" . $currentRow['apellidos'] . "</td>";
+		echo "</tr>";
+		}
+		?>
+	</table>
 </body>
 </html>
